56fa91e023ec4ab2243947cfba3e206e5dd84d5d
ano373 <andrewamin37@gmail.com>
2025-10-02 01:51:29 +0300

    feat(course) all api endpoints

--- src/api/CourseApi.ts
+++ src/api/CourseApi.ts
@@ -2,18 +2,19 @@ import { http } from "../lib/http";
 import type {
   CourseDetails,
   CoursePaginationParameter,
+  CourseRequest,
   CourseSummary,
 } from "../types/course";
 import type { ApiResponse } from "../types/general";
 
-async function fetchCourseById(
+export async function fetchCourseById(
   id: number
 ): Promise<ApiResponse<CourseDetails>> {
   const response = await http.get<ApiResponse<CourseDetails>>(`/courses/${id}`);
   return response.data;
 }
 
-async function fetchCourses(
+export async function fetchCourses(
   params?: CoursePaginationParameter
 ): Promise<ApiResponse<CourseSummary[]>> {
   const response = await http.get<ApiResponse<CourseSummary[]>>(`/courses`, {
@@ -22,15 +23,23 @@ async function fetchCourses(
   return response.data;
 }
 
-async function deleteCourse(id: number): Promise<void> {
+export async function deleteCourse(id: number): Promise<void> {
   await http.delete(`/courses/${id}`);
 }
 
 export async function addCourse(
-  payload: InstructorFormData
+  payload: CourseRequest
 ): Promise<ApiResponse<ApiResponse<CourseSummary>>> {
-  const response = await http.post(`/instructors`, payload, {
+  const response = await http.post(`/courses`, payload, {
     headers: { "Content-Type": "application/json" },
   });
+  return response.data;
+}
+
+export async function updateCourse(
+  payload: CourseRequest
+): Promise<ApiResponse<CourseSummary>> {
+  const response = await http.put(`/courses/${payload.courseId}`, payload);
+
   return response.data;
 }
